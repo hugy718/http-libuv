0108f233b173bc764f85b55672a6b1a99eebb34e
hugy718 <hugy718@gmail.com>
2022-05-11 14:52:43 +0800

    Add llhttp implmentation

--- Makefile
+++ Makefile
@@ -1,23 +1,36 @@
+# choose between llhttp and http_parser (default http_parser)
+USE_LLHTTP ?= 0
+
 COMMON_CXX_FLAGS := -g -O0 -Wall -Wextra
 # COMMON_CXX_FLAGS += -fsanitize=address
 
 DEPS_INSTALL_DIR = deps/install
 HTTP_PARSER_DIR = deps/http-parser
 LIBUV_DIR = $(DEPS_INSTALL_DIR)/libuv
+LLHTTP_DIR = $(DEPS_INSTALL_DIR)/llhttp
 
-COMMON_CXX_FLAGS += -I$(HTTP_PARSER_DIR) -I$(LIBUV_DIR)/include
+COMMON_LINK_FLAGS := $(COMMON_CXX_FLAGS) \
+	-L$(LIBUV_DIR)/lib -l:libuv_a.a -lpthread -ldl
 
-COMMON_LINK_FLAGS := $(COMMON_CXX_FLAGS)
-COMMON_LINK_FLAGS += -L$(HTTP_PARSER_DIR) -l:libhttp_parser.a \
-	-L$(LIBUV_DIR)/lib/ -l:libuv_a.a -lpthread -ldl
+COMMON_CXX_FLAGS += -I$(LIBUV_DIR)/include
+
+ifneq ($(USE_LLHTTP), 0)
+	COMMON_CXX_FLAGS += -DUSE_LLHTTP -I$(LLHTTP_DIR)/include
+	COMMON_LINK_FLAGS += -L$(LLHTTP_DIR)/lib -l:libllhttp.a
+else
+	COMMON_CXX_FLAGS += -I$(HTTP_PARSER_DIR)
+	COMMON_LINK_FLAGS += -L$(HTTP_PARSER_DIR) -l:libhttp_parser.a
+endif
 
 preparation:
 	git submodule update --init --recursive
 
 deps:
-	mkdir deps/install
+	mkdir $(DEPS_INSTALL_DIR)
 	cd deps/http-parser && make clean && make package && cd ../..
 	cd deps/libuv && mkdir build && cd build && cmake .. -DCMAKE_INSTALL_PREFIX=../../install/libuv && make && make install && cd ../../..
+	mkdir -p $(DEPS_INSTALL_DIR)/llhttp/include && mkdir $(DEPS_INSTALL_DIR)/llhttp/lib 
+	cd deps/llhttp && npm install && make && PREFIX=../install/llhttp make install && cd ../..
 
 .PHONY: preparation deps all clean mrproper
 
@@ -38,5 +51,6 @@ clean:
 
 mrproper: clean
 	make -C deps/http-parser clean
+	make -C deps/llhttp clean
 	rm -rf deps/libuv/build
 	rm -rf deps/install
--- main.cc
+++ main.cc
@@ -3,7 +3,18 @@
 #include <cstdlib>
 #include <cstring>
 #include "uv.h"
-#include "http_parser.h"
+
+#ifdef USE_LLHTTP
+  #include "llhttp.h"
+  typedef llhttp_t parser_t;
+  typedef llhttp_settings_t parser_settings_t;
+  #define PARSER_SETTINGS_INIT(m) llhttp_settings_init(m)
+#else // use http_parser
+  #include "http_parser.h"
+  typedef http_parser parser_t;  
+  typedef http_parser_settings parser_settings_t;
+  #define PARSER_SETTINGS_INIT(m) http_parser_settings_init(m)
+#endif // USE_LLHTTP
 
 #define RESPONSE \
   "HTTP/1.1 200 OK\r\n" \
@@ -19,12 +30,12 @@ uv_loop_t* loop;
 
 typedef struct {
   uv_tcp_t handle;
-  http_parser parser;
+  parser_t parser;
   uv_write_t write_req;
   uv_buf_t resbuf;
 } client_t;
 
-http_parser_settings settings;
+parser_settings_t settings;
 
 bool check_error(int ret) {
   if (!ret) return false;
@@ -44,19 +55,19 @@ void after_write(uv_write_t* req, int status) {
   uv_close((uv_handle_t*) req->handle, on_close);
 }
 
-int on_message_begin(http_parser* _) {
+int on_message_begin(parser_t* _) {
   (void)_;
   printf("\n***MESSAGE BEGIN***\n\n");
   return 0;
 }
 
-int on_headers_complete(http_parser* _) {
+int on_headers_complete(parser_t* _) {
   (void)_;
   printf("\n***HEADERS COMPLETE***\n\n");
   return 0;
 }
 
-int on_message_complete(http_parser* parser) {
+int on_message_complete(parser_t* parser) {
   printf("\n***MESSAGE COMPLETE***\n\n");
   client_t* client = (client_t*) parser->data;
   uv_write(&client->write_req, (uv_stream_t*) &client->handle,
@@ -64,25 +75,25 @@ int on_message_complete(http_parser* parser) {
   return 0;
 }
 
-int on_url(http_parser* _, const char* at, size_t length) {
+int on_url(parser_t* _, const char* at, size_t length) {
   (void)_;
   printf("Url: %.*s\n", (int)length, at);
   return 0;
 }
 
-int on_header_field(http_parser* _, const char* at, size_t length) {
+int on_header_field(parser_t* _, const char* at, size_t length) {
   (void)_;
   printf("Header field: %.*s\n", (int)length, at);
   return 0;
 }
 
-int on_header_value(http_parser* _, const char* at, size_t length) {
+int on_header_value(parser_t* _, const char* at, size_t length) {
   (void)_;
   printf("Header value: %.*s\n", (int)length, at);
   return 0;
 }
 
-int on_body(http_parser* _, const char* at, size_t length) {
+int on_body(parser_t* _, const char* at, size_t length) {
   (void)_;
   printf("Body: %.*s\n", (int)length, at);
   return 0;
@@ -92,7 +103,7 @@ int on_body(http_parser* _, const char* at, size_t length) {
 //  to print something demonstrating the processing of each phase.
 // on_message_complete is rewritten to send back response.
 void setup_http_parser_settings() {
-  memset(&settings, 0, sizeof(settings));
+  PARSER_SETTINGS_INIT(&settings);
   settings.on_message_begin = on_message_begin;
   settings.on_url = on_url;
   settings.on_header_field = on_header_field;
@@ -114,13 +125,24 @@ void on_read(uv_stream_t* stream, ssize_t nread, const uv_buf_t* buf) {
     printf("Read:\n%.*s\n", (int) nread, buf->base);
     /*parse http*/
     client_t* client = (client_t*) uv_handle_get_data((uv_handle_t*) stream);
-    http_parser* parser = &client->parser;
+    parser_t* parser = &client->parser;
+
+#ifdef USE_LLHTTP
+    auto ret = llhttp_execute(parser, buf->base, nread);
+    if (ret != HPE_OK) {
+      // fprintf(stderr, "Parse error: %s %s\n", llhttp_errno_name(ret),
+      //   parser->reason);
+      printf("Not a http request, no response sent\n");
+    }
+#else // use http_parser
     auto parsed = http_parser_execute(parser, &settings, buf->base, nread);
     if (parsed < static_cast<size_t>(nread)) {
       // fprintf(stderr, "parser error(%s): parsed lesser bytes %ld of %ld\n",
       //   http_errno_name(http_errno(parser->http_errno)), parsed, nread);
       printf("Not a http request, no response sent\n");
     }
+#endif // USE_LLHTTP 
+
   } else {
     // error
     if (nread != UV_EOF) {
@@ -128,6 +150,7 @@ void on_read(uv_stream_t* stream, ssize_t nread, const uv_buf_t* buf) {
     }
     uv_close((uv_handle_t*) stream, on_close);
     uv_stop(loop);
+    printf("server schedules to shutdown\n");
   }
 
   free(buf->base);
@@ -135,7 +158,7 @@ void on_read(uv_stream_t* stream, ssize_t nread, const uv_buf_t* buf) {
 
 void on_connection(uv_stream_t* server_handle, int status) {
   assert(server_handle == (uv_stream_t*) &server);
-  printf("on_connection\n");
+  printf("\non_connection\n");
 
   if (check_error(status)) return;
 
@@ -145,7 +168,12 @@ void on_connection(uv_stream_t* server_handle, int status) {
   client_t* client = (client_t*) malloc(sizeof(client_t));
 
   // init
+#ifdef USE_LLHTTP 
+  llhttp_init(&client->parser, HTTP_BOTH, &settings);
+#else // use http parser 
   http_parser_init(&client->parser, HTTP_REQUEST);
+#endif // USE_LLHTTP 
+
   auto ret = uv_tcp_init(server_handle->loop, &client->handle);
   // let the data pointer of handle to point to the client struct,
   //  so we can access http parser.
@@ -168,6 +196,7 @@ void on_connection(uv_stream_t* server_handle, int status) {
 } // anonymous namespace
 
 int main() {
+  printf("sample server launched\nWaiting for connections...\n");
  
   // setup http parser settings
   setup_http_parser_settings();
